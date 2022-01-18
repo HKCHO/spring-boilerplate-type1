ac1578a1cfd737276234e808139a4aaccc1b5aa1
hkcho <eddie88cho@gmail.com>
2022-01-18 23:20:57 +0900

    Spring application과 관련된 상수는 여기서 관리하기로 약속해. 제발.

---
+++ src/main/java/com/eddie88cho/backend/constants/AppConstants.java
@@ -0,0 +1,10 @@
+package com.eddie88cho.backend.constants;
+
+/**
+ * 어플리케이션 상수
+ */
+public class AppConstants {
+    public static final String DEFAULT_PACKAGE      = "com.eddie88cho.backend";
+    public static final String DAO_PACKAGE          = DEFAULT_PACKAGE + ".dao";
+    public static final String TYPE_ALIAS_PACKAGE   = DEFAULT_PACKAGE + ".domain";
+}
